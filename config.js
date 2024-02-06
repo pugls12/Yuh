d5e3ad95dffde1c05b52d013ac2d8164b9d97c72
pugls12 <120046993+pugls12@users.noreply.github.com>
2024-02-06 12:54:46 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://simple-proxy.discowd686.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODI3MjQ1ODY0YmVkMGZiNjMxYTg5NjIyMDI2NjU2OSIsInN1YiI6IjY1YzI2ZjFjYmJlMWRkMDE0NTJhN2FiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hfCYyEo229_ehSL1c3_gEyKj4GXNa5bNWwEA2L22azg",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
